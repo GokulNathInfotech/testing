1122aa0c7bfcd9d5a0e65023acc1d530a156c71a
GokulNath Developer <92971019+GokulNathInfotech@users.noreply.github.com>
2022-04-21 15:14:35 +0530

    c4

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <nav>
       <h1>Gokulnath</h1>
       <h1>frontend developer</h1>
-     <h6>Transform Tech</h6>
+     <h1>Transform Tech</h1>
+    <h3>cbe</h3>
       
       </nav>
     </div>
